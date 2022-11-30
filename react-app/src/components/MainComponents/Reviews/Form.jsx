68e0a51788ddb129051ac48329526b1e01bd50c5
Sergey <sergeyvinogorov@rambler.ru>
2022-11-30 15:48:46 +0300

    Выполнено Дз 30

--- react-app/src/components/MainComponents/Reviews/Form.jsx
+++ react-app/src/components/MainComponents/Reviews/Form.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { validate } from './utils/feedbackValidate';
+import { validate } from './ReviewItem/ReviewValidate';
 
 const storage = window.localStorage;
 
@@ -134,13 +134,3 @@ const AddCommentForm = () => {
 };
 
 export default AddCommentForm;
-
-var show;
-function hidetxt(type){
-param=document.getElementById(type);
-if(param.style.display == "none") {
-if(show) show.style.display = "none";
-param.style.display = "block";
-show = param;
-}else param.style.display = "none"
-}
